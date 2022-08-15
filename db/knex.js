4b04f46f946aa87af9f940f695cf0346b97fa4e3
10mo8 <10mo8since2020@gmail.com>
2022-08-15 15:07:23 +0900

    DB接続にSSLを有効にする

--- db/knex.js
+++ db/knex.js
@@ -11,7 +11,7 @@ const connect = () => {
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         port: process.env.DB_PORT,
-//        ssl: { rejectUnauthorized: false },
+        ssl: { rejectUnauthorized: false },
     };
 
     const knex = Knex({
